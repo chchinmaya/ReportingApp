eff505b8a29665a471606d0dfd53debcb1a0eaa1
chchinmaya <chundurichinmaya@gmail.com>
2016-06-23 17:40:55 +0530

    CustomFunctions DLL

---
+++ Customfunction/Customfunction/JsonFormatData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace Customfunction
+{
+    public class JsonFormatData
+    {
+        public static string GetData()
+        {
+            Zipcodes obj = new Zipcodes();
+            obj.ZipCode = "10001";
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+
+        public class Zipcodes
+        {
+            public string ZipCode { get; set; }
+        }
+    }
+}
